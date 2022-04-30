ff61ad47c1a3de52c30c34f806224fd0694adecf
Alvin <alxiao@ucsd.edu>
2022-04-29 17:52:41 -0700

    created files

--- SkillDemoOne.java
+++ SkillDemoOne.java
@@ -1,3 +1,5 @@
 class SkillDemoOne {
-
+    public static int multiplyByTwo(int n) {
+        return n * 2;
+    }
 }
---
+++ SkillDemoOneTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoOneTest {
+    @Test
+    public void testOne() {
+        assertEquals(3, SkillDemoOne.multiplyByTwo(2));
+    }
+}
